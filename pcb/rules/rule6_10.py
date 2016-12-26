7a16cbf64dc7de73b9bd4bbdc25a6daf3e8765d1
Piotr Wiszowaty <pwiszowaty458@gmail.com>
2016-12-26 19:11:17 +0100

    Change values thickness to 0.1 mm

--- pcb/rules/rule6_10.py
+++ pcb/rules/rule6_10.py
@@ -7,19 +7,19 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad_mod files.
     """
     def __init__(self, module):
-        super(Rule, self).__init__(module, 'Rule 6.9', 'Reference has a height of 1mm, width of 1mm and thickness of 0.12mm.')
+        super(Rule, self).__init__(module, 'Rule 6.10', 'Value has a height of 1mm, width of 1mm and thickness of 0.1mm.')
         self.height = 1
         self.width = 1
-        self.thickness = 0.12
+        self.thickness = 0.1
 
     def check(self):
         """
         Proceeds the checking of the rule.
         """
         module = self.module
-        if (module.reference['font']['height'] != self.height or
-            module.reference['font']['width'] != self.width or
-            module.reference['font']['thickness'] != self.thickness):
+        if (module.value['font']['height'] != self.height or
+            module.value['font']['width'] != self.width or
+            module.value['font']['thickness'] != self.thickness):
            return True
 
         return False
@@ -30,6 +30,6 @@ class Rule(KLCRule):
         """
         module = self.module
         if self.check():
-            module.reference['font']['height'] = self.height
-            module.reference['font']['width'] = self.width
-            module.reference['font']['thickness'] = self.thickness
+            module.value['font']['height'] = self.height
+            module.value['font']['width'] = self.width
+            module.value['font']['thickness'] = self.thickness
