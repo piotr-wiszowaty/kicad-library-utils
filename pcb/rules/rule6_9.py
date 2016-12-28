ca78efd0dd65f369c1116a5093417752b8187e36
Piotr Wiszowaty <pwiszowaty458@gmail.com>
2016-12-28 11:01:52 +0100

    Revert "Fix rule 6.9"
    This reverts commit a5a3264f5648b568b6576077550e35b3a6a9ec7f.

--- pcb/rules/rule6_9.py
+++ pcb/rules/rule6_9.py
@@ -7,19 +7,16 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad_mod files.
     """
     def __init__(self, module):
-        super(Rule, self).__init__(module, 'Rule 6.9', 'Value and reference have a height of 1mm, width of 1mm and thickness of 0.12mm.')
-        self.height = 1
-        self.width = 1
-        self.thickness = 0.12
+        super(Rule, self).__init__(module, 'Rule 6.9', 'Value and reference have a height of 1mm.')
 
     def check(self):
         """
         Proceeds the checking of the rule.
         """
         module = self.module
-        if (module.reference['font']['height'] != self.height or module.value['font']['height'] != self.height or
-            module.reference['font']['width'] != self.width or module.value['font']['width'] != self.width or
-            module.reference['font']['thickness'] != self.thickness or module.value['font']['thickness'] != self.thickness):
+        if (module.reference['font']['height'] != 1 or module.value['font']['height'] != 1 or
+            module.reference['font']['width'] != 1 or module.value['font']['width'] != 1 or
+            module.reference['font']['thickness'] != 0.15 or module.value['font']['thickness'] != 0.15):
            return True
 
         return False
@@ -30,10 +27,10 @@ class Rule(KLCRule):
         """
         module = self.module
         if self.check():
-            module.reference['font']['height'] = self.height
-            module.reference['font']['width'] = self.width
-            module.reference['font']['thickness'] = self.thickness
+            module.reference['font']['height'] = 1
+            module.reference['font']['width'] = 1
+            module.reference['font']['thickness'] = 0.15
 
-            module.value['font']['height'] = self.height
-            module.value['font']['width'] = self.width
-            module.value['font']['thickness'] = self.thickness
+            module.value['font']['height'] = 1
+            module.value['font']['width'] = 1
+            module.value['font']['thickness'] = 0.15
